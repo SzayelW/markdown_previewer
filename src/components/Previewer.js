da1e927695549d422e8b2163ca54cdb855733151
Souren <andres70p32@gmail.com>
2018-10-11 13:00:56 -0500

    Se agrego paquete marked para la conversion de texto a html

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import marked from 'marked';
 
 export default class Previewer extends React.Component{
   render(){
     return (
-      <div>
-        {this.props.text}
-      </div>
+      <div  dangerouslySetInnerHTML={ {__html: marked(this.props.text) }} />
     );
   }
 }
